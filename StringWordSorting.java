d0f7dacce22ba0a28bdfadfd34bd05aae8b33111
Alokmish11 <140834850+Alokmish11@users.noreply.github.com>
2024-05-12 16:26:55 +0530

    Add files via upload

---
+++ StringWordSorting.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class StringWordSorting {
+  public static String solve(String str) {
+    char[] char_arr = str.toCharArray();
+    Arrays.sort(char_arr);
+    // Creating object of String class
+    String ans = new String(char_arr);
+    return ans;
+  }
+  public static void main(String args[]) {
+    String str = "zxcbg";
+
+    System.out.println("String after sorting:");
+    System.out.println(solve(str));
+  }
+}
+    
+
