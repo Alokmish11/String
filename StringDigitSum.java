ae4f06c89156358aead3ec5cb7baa2e463884185
Alokmish11 <140834850+Alokmish11@users.noreply.github.com>
2024-05-12 16:27:18 +0530

    Add files via upload

---
+++ StringDigitSum.java
@@ -0,0 +1,33 @@
+
+
+    import java.util.*;
+    public class StringDigitSum {
+
+       static int findSum(String str) {
+              String tempSum = "0";
+              int sum = 0;
+              for (int i = 0; i < str.length(); i++) {
+                     char s = str.charAt(i);
+
+                     if (Character.isDigit(s))
+                            tempSum += s;
+
+                     else {
+                            sum += Integer.parseInt(tempSum);
+                            tempSum = "0";
+                     }
+              }
+
+              return sum + Integer.parseInt(tempSum);
+       }
+
+       public static void main(String[] args) {
+
+              String str = "1a2b3c";
+              System.out.print("Sum: ");
+              System.out.println(findSum(str));
+       }
+}
+
+    
+
